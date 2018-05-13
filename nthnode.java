274f355fb8ba662ea5d42f3f62851375b1d78c4a
Kiran Ashok <stepinfwd@gmail.com>
2018-05-13 14:03:06 +0530

    Update nthnode.java

--- nthnode.java
+++ nthnode.java
@@ -1,4 +1,38 @@
 
+//   question             
+//Given a linked list, the task is to find the n'th node from the end.  
+//  It is needed to complete a method that takes two argument, head of 
+//  linked list and an integer n. There are multiple test cases. For each test case, this method will be called individually.
+    
+/*output
+
+
+Input:
+The first line of input contains number of test cases.  Every test case cntains two lines.  First line of every test case contains two space separated values, number of nodes  in linked list followed by value of n.  Second line of every test case contains data items of linked list.
+
+
+Output:
+Corresponding to each test case, output a single integer that is the nth integer in the linked list from the end. Print -1 if the value of n is greater than the number of elements in the linked list.
+
+Constraints:
+1 <= T <= 50
+0 <= No of Nodes <= 1000
+0 <= Data in Nodes <= 1000
+
+
+Example:
+Input:
+2
+9 2
+1 2 3 4 5 6 7 8 9
+4 5
+10 -5 -100 5 
+ 
+
+Output:
+8
+-1
+*/
 
 import java.io.*;
 import java.util.*;
