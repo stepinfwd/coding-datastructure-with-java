3d99cb80448684e013177b132da4472b3a31b03c
Kiran Ashok <stepinfwd@gmail.com>
2018-05-13 14:11:54 +0530

    Update middle.java

--- middle.java
+++ middle.java
@@ -1,3 +1,36 @@
+/* finding middle element of linked list and reversing a linked list 
+                          
+Given a singly linked list, find middle of the linked list. For example, if given linked list is 1->2->3->4->5 
+then output should be 3. 
+
+If there are even nodes, then there would be two middle nodes, we need to print second middle element. For example, 
+if given linked list
+is 1->2->3->4->5->6 then output should be 4
+--------------------------------------------------------------------
+                                
+Given pointer to the head node of a linked list, the task is to reverse the linked list.
+
+Input:
+You need to complete a method reverse() that takes head as argument and returns new head.
+There are multiple test cases. For each test case, this method will be called individually.  The first line of input contains number of test cases.  Every test case has two lines, number of nodes first line and data values in next line.
+
+Output:
+Reverse the linked list and return head of the modified list.
+
+
+Example:
+Input:
+1
+6
+1 2 3 4 5 6
+
+Output:
+6 5 4 3 2 1
+
+
+
+
+*/
 import java.util.*;
 import java.io.*;
  class node
